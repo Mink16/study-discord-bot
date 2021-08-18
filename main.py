928db46d94b36c4b6d655d5b72f6c3db1099f56c
Mink16 <minomax16@yahoo.co.jp>
2021-08-18 23:37:49 +0900

    (仮)特定のリアクションを付けると特定の役職を付けたり外したりできるようにする

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 import discord
 import requests
-from discord import CategoryChannel, Guild
+from discord import CategoryChannel, Guild, RawReactionActionEvent
 from discord.channel import VoiceChannel
 from discord.ext import commands
 from discord.member import Member, VoiceState
@@ -16,7 +16,9 @@ BASE_URL = 'https://discord.com/api'
 API_TOKEN = os.getenv('API_TOKEN')
 TARGET_V_CHANNEL_ID = os.getenv('TARGET_V_CHANNEL_ID')
 INVITE_CHANNEL_ID = os.getenv('INVITE_CHANNEL_ID')
-INVITE_ROLE = os.getenv('INVITE_ROLE')
+INVITE_ROLE_ID = os.getenv('INVITE_ROLE_ID')
+ADD_ROLE_CHANNEL_ID = os.getenv('ADD_ROLE_CHANNEL_ID')
+ADD_ROlE_EMOJI = os.getenv('ADD_ROlE_EMOJI')
 RECOG_STR = os.getenv('RECOG_STR')
 
 headers = {
@@ -62,8 +64,32 @@ async def on_voice_state_update(user: Member, old: VoiceState, new: VoiceState):
 
 			# 募集をかける
 			invite: discord.Invite = await buntai_channel.create_invite()
-			r = requests.post(f'{BASE_URL}/channels/{INVITE_CHANNEL_ID}/messages', json={'content': INVITE_ROLE + '\n' + invite.url}, headers=headers)
+			r = requests.post(f'{BASE_URL}/channels/{INVITE_CHANNEL_ID}/messages', json={'content': '<@&' + INVITE_ROLE_ID + '>\n' + invite.url}, headers=headers)
 
 			bot_db.insert_messages(client.user.id, int(r.json()['id']), int(INVITE_CHANNEL_ID), buntai_channel.id)
+
+# リアクション追加時
+@client.event
+async def on_raw_reaction_add(payload: RawReactionActionEvent):
+	if payload.emoji.name != ADD_ROlE_EMOJI \
+		or payload.channel_id != int(ADD_ROLE_CHANNEL_ID): 
+		return
+	await change_role(payload, True)
+
+# リアクション削除時
+@client.event
+async def on_raw_reaction_remove(payload: RawReactionActionEvent):
+	if payload.emoji.name != ADD_ROlE_EMOJI \
+		or payload.channel_id != int(ADD_ROLE_CHANNEL_ID): 
+		return
+	await change_role(payload, False)
+
+async def change_role(payload: RawReactionActionEvent, role_flg: bool):
+	guild: Guild = client.get_guild(payload.guild_id)
+	member: Member = guild.get_member(payload.user_id)
+
+	# 役職を編集
+	role = guild.get_role(int(INVITE_ROLE_ID))
+	await member.add_roles(role) if role_flg else await member.remove_roles(role)
 
 client.run(API_TOKEN)
